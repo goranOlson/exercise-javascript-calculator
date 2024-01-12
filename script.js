97abda3ef855667f42ce6b9a2a10537f26475f8e
Göran Olson <goran@famolson.se>
2024-01-12 11:15:25 +0100

    Removed unimportant comments

--- script.js
+++ script.js
@@ -1,26 +1,19 @@
-// 1) Ask for a number
+// Ask user for two numbers and the calculation method
 const number1 = prompt("Please give a number");
-// const number1 = "9";
-
-// 2) Ask for a number
 const number2 = prompt("Please give another number");
-// const number2 = 3;
-
-// 3) Ask for calculation method
 const method = prompt("Which calculation method do you want to use? Choose one of: +, -, *, /");
-// const method = "/";
-// Check that methoc is correct
+
+// Check that method is correct
 const isMethod = (method === '+' || method === '-' || method === '*' || method === '/');
 
-// 4) Calculate data if correct
+// Get the numerical value of the input numbers
 const nbr1 = parseInt(number1);
 const nbr2 = parseInt(number2);
 // console.log(`${nbr1} ${nbr2} '${method}' ${isMethod}`);
 
 if (isNaN(nbr1) === false && isNaN(nbr2) === false && isMethod === true) {
     let result;
-    // 
-    console.log('Data ok');
+    
     if (method === '+') {
         result = nbr1 + nbr2;
     }
